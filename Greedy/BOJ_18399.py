1a7d0cbec3cbd56bac29e272b2a32b5f47a32be6
99sphere <59161083+99sphere@users.noreply.github.com>
2021-07-24 19:10:27 +0900

    Add files via upload

---
+++ Greedy/BOJ_18399.py
@@ -0,0 +1,12 @@
+# BOJ 18399
+# Author: Gu Lee
+# Date: 2021.07.23
+# Source: https://www.acmicpc.net/problem/11399
+
+n = int(input())
+nums = list(map(int, input().split()))
+nums = sorted(nums)
+result = 0
+for i in range(len(nums)):
+  result += sum(nums[:i+1])
+print(result)
