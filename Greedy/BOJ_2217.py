84fe33e3e8f4501487c9ee456d44211a9633156f
Gu Lee <59161083+99sphere@users.noreply.github.com>
2021-08-13 03:46:04 +0900

    Add files via upload

---
+++ Greedy/BOJ_2217.py
@@ -0,0 +1,15 @@
+# BOJ 2217
+# Author: Gu Lee
+# Date: 2021.07.13
+# Source: https://www.acmicpc.net/problem/2217
+
+n = int(input())
+ropes = []
+for i in range(n):
+  ropes.append(int(input()))
+ropes = sorted(ropes)
+cand = []
+for i in range(n):
+    cand.append(ropes[i]*(n-i))
+result = max(cand)
+print(result)
